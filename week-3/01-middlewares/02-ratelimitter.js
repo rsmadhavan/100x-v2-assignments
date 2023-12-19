a297be1e0f95ac05e97272e27702dfbae7e2fe17
rsmadhavan <rsmadhavan2000@gmail.com>
2023-12-20 00:47:10 +0530

    Solve week 4 0-1 middlewares   app.use((req,res,next)=>{}) -- middlewares   app.use((error,req,res,next)=>{}) -- global catches

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,6 +1,6 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
 const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
@@ -13,15 +13,27 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+  numberOfRequestsForUser = {};
+}, 1000);
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.use((req, res, next) => {
+  const user = "user"; //req.headers.user-id;
+  if (numberOfRequestsForUser[user]>=0) {
+    numberOfRequestsForUser[user] += 1;
+  } else {
+    numberOfRequestsForUser[user] = 0;
+  }
+  if (numberOfRequestsForUser[user] >= 5) {
+    return res.status(404).send();
+  }
+  next();
+});
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
 module.exports = app;
